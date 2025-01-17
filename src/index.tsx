9f81e1e5a5a5c3cb091d40d26ee703f915a0f0ab
fedpre <fed.pregnolato@gmail.com>
2025-01-17 17:53:25 +0100

    chore(lib): added react native reanimated dependency

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,9 @@
+import { View } from 'react-native';
+
 export function multiply({ n1, n2 }: { n1: number; n2: number }): number {
   return n1 * n2;
 }
+
+export const Test = () => {
+  return <View />;
+};
