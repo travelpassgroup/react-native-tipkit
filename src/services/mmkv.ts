e79fb2e58f144b0e041dc987afab599ee828a0bc
Federico <82700080+fedpre@users.noreply.github.com>
2025-02-03 19:01:46 +0100

    fix: mmkv storage issue (#22)

--- src/services/mmkv.ts
+++ src/services/mmkv.ts
@@ -1,7 +1,6 @@
-import { MMKV, Mode } from 'react-native-mmkv';
+import { MMKV } from 'react-native-mmkv';
 
 export const storage = new MMKV({
   id: 'tipkit',
   encryptionKey: 'tipkit-encryption-key',
-  mode: Mode.MULTI_PROCESS,
 });
